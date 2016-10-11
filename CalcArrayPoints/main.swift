378b5676d4eed93d9e64e45bb217cb07155da955
zoexiong <xiongsy@uw.edu>
2016-10-11 12:41:40 -0700

    add error handling for Cal

--- CalcArrayPoints/main.swift
+++ CalcArrayPoints/main.swift
@@ -8,35 +8,55 @@
 
 import Foundation
 
-func add(left:Float, right:Float) -> Float{
+
+//Cal
+func add(left:Int, right:Int) -> Int{
     let result = left + right
     return result
 }
 
-func subtract(left:Float, right:Float) -> Float{
+func subtract(left:Int, right:Int) -> Int{
     let result = left - right
     return result
 }
 
-func multiply(left:Float, right:Float) -> Float{
+func multiply(left:Int, right:Int) -> Int{
     let result = left * right
     return result
 }
 
-func divide(left:Float, right:Float) -> Float? {
-    if right != 0 {
-  let result = left / right
-        return result
-    } else {
-       print("Invalid Input, right can not be 0")
-        return nil
-    }
-}
+//Below code can be used for x/0 error handling, but needs to be unwraped before use the result
 
-func mathOperation(left: Float, right: Float, operation: (Float, Float) -> Float?) -> Float? {
+//func divide(left:Int, right:Int) -> Int? {
+//    if right != 0 {
+//  let result = left / right
+//        return result
+//    } else {
+//       print("Invalid Input, right can not be 0")
+//        return nil
+//    }
+//}
+
+//func mathOperation(left: Int, right: Int, operation: (Int, Int) -> Int?) -> Int? {
+//    return operation(left, right)!
+//}
+
+func divide(left:Int, right:Int) -> Int {
+           let result = left / right
+        return result
+  }
+
+func mathOperation(left: Int, right: Int, operation: (Int, Int) -> Int) -> Int {
     return operation(left, right)
 }
 // why the labels "left:" and "right:" are not needed here?
+
+let addGenericResult = mathOperation(left: 4, right: 5, operation: add)
+print(addGenericResult)
+
+//Array
+
+
 
 
 
